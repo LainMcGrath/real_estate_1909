0d9bf1865bb2c77496f95bd1993adbbf1a7db108
Lain McGratH <lain.mcgrath@gusto.com>
2019-10-09 10:18:49 -0600

    Add house area and room categories

--- lib/house.rb
+++ lib/house.rb
@@ -1,6 +1,6 @@
 class House
 
-  attr_reader :price, :address, :rooms
+  attr_reader :price, :address, :rooms, :rooms_from_category, :area
 
   def initialize(price, address)
     @price = price
@@ -10,5 +10,17 @@ class House
 
   def add_room(room)
     @rooms << room
+  end
+
+  def rooms_from_category(category)
+    @rooms.find_all do |room|
+      room.category == category
+    end
+  end
+
+  def area
+    @rooms.reduce(0) do |house_area, room|
+      house_area += room.area
+    end
   end
 end
