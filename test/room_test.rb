6076bd7b9b864b46bd3959f6243dd20290bb5bfb
Lain McGratH <lain.mcgrath@gusto.com>
2019-10-09 09:21:00 -0600

    Add completed room test

--- test/room_test.rb
+++ test/room_test.rb
@@ -6,9 +6,26 @@ require './lib/room'
 class RoomTest< Minitest::Test
 
   def setup
-    room = Room.new(:bedroom, 10, 13)
+    @room = Room.new(:bedroom, 10, 13)
   end
 
   def test_it_exists
-    assert_instance_of Room, room
+    assert_instance_of Room, @room
   end
+
+  def test_it_has_a_category
+    assert_equal :bedroom, @room.category
+  end
+
+  def test_it_has_a_length
+    assert_equal 10, @room.length
+  end
+
+  def test_it_has_a_width
+    assert_equal 13, @room.width
+  end
+
+  def test_it_has_an_area
+    assert_equal 130, @room.area
+  end
+end
