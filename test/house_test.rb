c225deabe18b50824901f7dde1300ad6b9f82f8b
Lain McGratH <lain.mcgrath@gusto.com>
2019-10-09 11:00:27 -0600

    Add house test price per square foot

--- test/house_test.rb
+++ test/house_test.rb
@@ -6,7 +6,7 @@ require './lib/house'
 class HouseTest< Minitest::Test
 
   def setup
-    @house = House.new("$400000", "123 sugar lane")
+    @house = House.new("400000.00", "123 sugar lane")
     @room_1 = Room.new(:bedroom, 10, 13)
     @room_2 = Room.new(:bedroom, 11, 15)
   end
@@ -16,7 +16,7 @@ class HouseTest< Minitest::Test
   end
 
   def test_it_has_a_price
-    assert_equal "$400000", @house.price
+    assert_equal "400000.00", @house.price
   end
 
   def test_it_has_an_address
@@ -28,10 +28,17 @@ class HouseTest< Minitest::Test
     @house.add_room(@room_2)
     assert_equal [@room_1, @room_2], @house.rooms
   end
-  
+
   def test_it_can_calculate_house_area
     @house.add_room(@room_1)
     @house.add_room(@room_2)
     assert_equal 295, @house.area
+  end
+
+  def test_it_can_calculate_price_per_square_foot
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+
+    assert_equal 1355.93, @house.price_per_square_foot
   end
 end
